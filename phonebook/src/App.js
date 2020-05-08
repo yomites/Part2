21df1ce5e0e5f15c8d85996cedb2210f54179068
yomites <marshall_400@yahoo.com>
2020-05-09 02:30:26 +0300

    Update App.js
    Exercise 2.7: The Phonebook Step 2. The application now prevents the user from adding a duplicate name to the phonebook and it is also case insensitive so that as long as the letters in 2 names match, the duplicate name won't be added.

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -12,16 +12,27 @@ const App = () => {
     const personObject = {
       name: newName,
     }
-
-    setPersons(persons.concat(personObject))
-    setNewName('')
+//    console.log('Persons and new name:', persons, newName
+        setPersons(persons.concat(personObject))
+        setNewName('') 
   }
 
   const handleNameChange = (event) => {
-    console.log(event.target.value)
-    setNewName(event.target.value)
-  }
+    console.log('handleNameChange persons',persons)
+    console.log('handleNameChange', event.target.value)
+    const arr = persons.filter(element => 
+      element.name.toUpperCase()===event.target.value.toUpperCase())
 
+      if (arr.length===1) {
+        console.log('Arr array', arr)
+        const newName = arr[0].name
+        window.alert(newName + ' is already added to phonebook')
+      } else
+      return (
+      setNewName(event.target.value) 
+      )    
+  }
+ 
   const Person = ({ person }) => {
     console.log('Person:', person)
     return (
