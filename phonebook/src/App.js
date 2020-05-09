9db60ddf0fa6689490fc7f4a1ec90debe56f745b
yomites <marshall_400@yahoo.com>
2020-05-10 01:25:21 +0300

    Update App.js
    Exercise 2.9: The Phonebook Step 4. Now the application includes a search field to filter the list of people by name.

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -3,11 +3,17 @@ import './App.css';
 
 const App = () => {
   const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas', number: '040-1234567'}
+    { name: 'Arto Hellas', number: '040-1234567'},
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
   ]) 
   const [ newName, setNewName ] = useState('')
   const [ mobileNumber, setMobileNumber ] = useState('')
-  console.log('The person added:', persons)
+  const [ nameSearch, setNameSearch ] = useState('')
+  const [ nameToShow, setNameToShow ] = useState(persons)
+
+  console.log('The persons to show:', nameToShow)
 
   const addPerson = (event) => {
     event.preventDefault()
@@ -16,14 +22,32 @@ const App = () => {
       name: newName,
       number: mobileNumber,
     }
-//    console.log('Persons and new name:', persons, newName
+    console.log('Persons and new name:', persons, newName)
         setPersons(persons.concat(personObject))
+        setNameToShow(nameToShow.concat(personObject))
         setNewName('')
         setMobileNumber('')
   }
 
+  const myArray = (anArray, evt) => {
+    const arr = anArray.filter(element => 
+      element.name.toUpperCase().includes
+      (evt.target.value.toUpperCase()))
+    return arr
+  }
+
+  const handleNameSearchChange = (event) => {
+    console.log('handleNameSearchChange', 
+    event.target.value)
+    setNameSearch(event.target.value)
+    myArray(persons, event)
+    
+    setNameToShow(myArray(persons, event))
+  }
+
   const handleMobileNumberChange = (evt) => {
-    console.log('handleMobileNumberChange', evt.target.value)
+    console.log('handleMobileNumberChange', 
+    evt.target.value)
     setMobileNumber(evt.target.value) 
   }
 
@@ -31,12 +55,14 @@ const App = () => {
     console.log('handleNameChange persons',persons)
     console.log('handleNameChange', event.target.value)
     const arr = persons.filter(element => 
-      element.name.toUpperCase()===event.target.value.toUpperCase())
+      element.name.toUpperCase()===
+      event.target.value.toUpperCase())
 
       if (arr.length===1) {
         console.log('Arr array', arr)
         const newName = arr[0].name
-        window.alert(newName + ' is already added to phonebook')
+        window.alert(newName + 
+          ' is already added to phonebook')
       } else
       return (
       setNewName(event.target.value) 
@@ -55,14 +81,22 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+      filter shown with<input type="text" value={nameSearch}
+          placeholder="Enter search"
+          onChange={handleNameSearchChange} />
+      </div>
       <form onSubmit={addPerson}>
+        <h2>add a new</h2>
         <div>
           name: <input type="text" value={newName} 
-          placeholder="Enter name here" onChange={handleNameChange} />
+          placeholder="Enter name here" 
+          onChange={handleNameChange} />
         </div>
         <div>
           number: <input type="text" value={mobileNumber}
-          placeholder="Enter phone number here" onChange={handleMobileNumberChange} /></div>
+          placeholder="Enter phone number here" 
+          onChange={handleMobileNumberChange} /></div>
         <div>
           <button type="submit">
             add
@@ -70,7 +104,7 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(person => 
+      {nameToShow.map(person => 
         <Person key={person.name} person={person} />
       )}
     </div>
